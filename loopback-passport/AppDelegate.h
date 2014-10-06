6fe22928ec76bc5e7ebfa1ca1bb92167cbcea074
Yuumi Yoshida <yy@ey-office.com>
2014-10-06 23:08:47 +0900

    Initial Commit

---
+++ loopback-passport/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  loopback-passport
+//
+//  Created by Yuumi Yoshida on 2014/10/06.
+//  Copyright (c) 2014年 Yuumi Yoshida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ loopback-passport/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  loopback-passport
+//
+//  Created by Yuumi Yoshida on 2014/10/06.
+//  Copyright (c) 2014年 Yuumi Yoshida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
